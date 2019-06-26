dde3c15e41a15543eaa62158fa52efabe5e0bc75
phxrocker <25048484+phxrocker@users.noreply.github.com>
2019-06-26 08:51:36 -0700

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,28 +1,43 @@
-name = input('What is your name? ' )
-color = input('\nHi ' + name + "! What's your favorite color? ")
+def main():
 
-# Color Condition
-while color == 'blue':
-    print('\n' + color + "?! That's awesome " + name + "!! Blue's my favorite color too!!")
-    break
-else:
-    print('\n' + color + "'s ok... i guess ")
+  name = input('What is your name? ' )
+  color = input('\nHi ' + name + "! What's your favorite color? ")
 
-eye_color = input('\nWhat color are your eyes? ')
+  # Color Condition
+  while color == 'blue':
+      print('\n' + color + "?! That's awesome " + name + "!! Blue's my favorite color too!!")
+      break
+  else:
+      print('\n' + color + "'s ok... i guess ")
 
-# Eye Color Condition
-while eye_color != color:
-    print('\nNo way,' + name + '! I would think that ' + eye_color + ' would be your favorite color then!')
-    break
-else:
-    print('\nFigures')
+  eye_color = input('\nWhat color are your eyes? ')
 
-car_check = input('\nDo you have a car, yes or no? ')
+  # Eye Color Condition
+  while eye_color != color:
+      print('\nNo way,' + name + '! I would think that ' + eye_color + ' would be your favorite color then!')
+      break
+  else:
+      print('\nFigures')
 
-# Did the user have a car
-while car_check == 'yes':
+  car_check = input('\nDo you have a car, yes or no? ')
+
+  # Did the user have a car
+  while car_check == 'yes':
     car_color = input('\nWell good for you.  What color is it? ')
-else:
-    print("\nThat's ok, " + name + ".  We'll skip that one for now")
+    break
+  else:
+      print("\nThat's ok, " + name + ".  We'll skip that one for now")
+  
+  # Car Color Condition
+  while car_color == color:
+    print('\nOf course it is\n')
+    break
+  else:
+    main()   
+
+main()
+
+if __name__ == '__main__':
+    main()
 
 input()
