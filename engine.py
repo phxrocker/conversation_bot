becc4c507d1f00b7803a039739227c447c3988a4
phxrocker <25048484+phxrocker@users.noreply.github.com>
2019-06-26 22:42:54 -0700

    Update engine.py
    Final commit.  Lesson learned about infinite loops and proper use of 'return'

--- engine.py
+++ engine.py
@@ -4,42 +4,34 @@ def main():
   color = input('\nHi ' + name + "! What's your favorite color? ")
 
   # Color Condition
-  while color == 'blue':
+  if color == 'blue':
       print('\n' + color + "?! That's awesome " + name + "!! Blue's my favorite color too!!")
-      break
   else:
       print('\n' + color + "'s ok... i guess ")
 
   eye_color = input('\nWhat color are your eyes? ')
 
   # Eye Color Condition
-  while eye_color != color:
+  if eye_color != color:
       print('\nNo way,' + name + '! I would think that ' + eye_color + ' would be your favorite color then!')
-      break
   else:
       print('\nFigures')
 
   car_check = input('\nDo you have a car, yes or no? ')
 
   # Did the user have a car
-  while car_check == 'yes':
+  if car_check == 'yes':
     car_color = input('\nWell good for you.  What color is it? ')
-    break
   else:
-      print("\nThat's ok, " + name + ".  We'll skip that one for now\n")
-      main()  
+    print("\nThat's ok, " + name + ".  We'll skip that one for now\n")
+    return 
 
   # Car Color Condition
-  while car_color == color:
+  if car_color == color:
     print('\nOf course it is\n')
-    main()
-    break
   else:
-    main()   
+    print('\n')
+    return  
 
-main()
-
-if __name__ == '__main__':
+while True:
     main()
-
-input()
